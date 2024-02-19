7dcb4b556bd90967aa057a459330fca91f3120c5
LAPTOP-650GLE2O\fanla <gpozo@digitalmarketing.cl>
2024-02-18 23:35:16 -0300

    br sm:hidden footer

--- src/components/layout/Footer.jsx
+++ src/components/layout/Footer.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 export const Footer = () => {
   return (
     <footer className='bg-summer-green-400 py-11'>
-      <p className='text-center text-summer-green-50 text-2xl font-thin'><strong className='text-4xl font-extrabold'>Gracias</strong> por visitar mi portafolio 😺</p>
+      <p className='text-center text-summer-green-50 text-2xl font-thin'><strong className='text-4xl font-extrabold'>Gracias</strong><br className='sm:hidden' /> por visitar mi portafolio 😺</p>
     </footer>
   )
 }
